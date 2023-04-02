0cde1e8da344f2d6a21693bd4d3358e9faad4703
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-02 22:26:25 +0200

    convert one to one KO

--- src/test/java/com/codurance/katalyst/FizzBuzzTest.java
+++ src/test/java/com/codurance/katalyst/FizzBuzzTest.java
@@ -1,5 +1,13 @@
 package com.codurance.katalyst;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzzTest {
     
+    @Test
+    public void convert_one_to_one(){
+        assertEquals("1", new FizzBuzz().convert(1));
+    }
 }
