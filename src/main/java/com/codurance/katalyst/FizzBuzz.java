65e1dc894a9691995512bc28e72eea3caacaf4f0
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-02 22:57:57 +0200

    convert twenty to buzz test OK

--- src/main/java/com/codurance/katalyst/FizzBuzz.java
+++ src/main/java/com/codurance/katalyst/FizzBuzz.java
@@ -4,16 +4,14 @@ public class FizzBuzz {
     
     private static int FIZZ_MODULE = 3;
     private static String FIZZ_VALUE = "fizz";
+    private static int BUZZ_MODULE = 5;
     private static String BUZZ_VALUE = "buzz";
 
     public String convert(int value){
         if(value % FIZZ_MODULE == 0){
             return FIZZ_VALUE;
         }
-        if(value == 5){
-            return BUZZ_VALUE;
-        }
-        if(value == 10){
+        if(value % BUZZ_MODULE == 0){
             return BUZZ_VALUE;
         }
         return "" + value;
