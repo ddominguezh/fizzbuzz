20122fef09ff3e43a745e06e0058582d12814512
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-02 22:38:15 +0200

    convert three to fizz test OK

--- src/main/java/com/codurance/katalyst/FizzBuzz.java
+++ src/main/java/com/codurance/katalyst/FizzBuzz.java
@@ -2,7 +2,13 @@ package com.codurance.katalyst;
 
 public class FizzBuzz {
     
+    private static int FIZZ_MODULE = 3;
+    private static String FIZZ_VALUE = "fizz";
+
     public String convert(int value){
-        return "1";
+        if(value % FIZZ_MODULE == 0){
+            return FIZZ_VALUE;
+        }
+        return "" + value;
     }
 }
