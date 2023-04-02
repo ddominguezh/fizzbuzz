62ae80c793c953f17b13f2b5453f1768c08ce08b
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-02 22:30:23 +0200

    convert one to one test OK

--- src/main/java/com/codurance/katalyst/FizzBuzz.java
+++ src/main/java/com/codurance/katalyst/FizzBuzz.java
@@ -3,6 +3,6 @@ package com.codurance.katalyst;
 public class FizzBuzz {
     
     public String convert(int value){
-        throw new UnsupportedOperationException("not implement!");
+        return "1";
     }
 }
