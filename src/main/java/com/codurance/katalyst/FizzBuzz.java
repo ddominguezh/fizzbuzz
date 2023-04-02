811f016e8137c8d71252ccd4cd14f1608e2594bc
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-02 22:54:26 +0200

    convert five to buzz test OK

--- src/main/java/com/codurance/katalyst/FizzBuzz.java
+++ src/main/java/com/codurance/katalyst/FizzBuzz.java
@@ -4,10 +4,14 @@ public class FizzBuzz {
     
     private static int FIZZ_MODULE = 3;
     private static String FIZZ_VALUE = "fizz";
+    private static String BUZZ_VALUE = "buzz";
 
     public String convert(int value){
         if(value % FIZZ_MODULE == 0){
             return FIZZ_VALUE;
+        }
+        if(value == 5){
+            return BUZZ_VALUE;
         }
         return "" + value;
     }
