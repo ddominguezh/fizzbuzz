9e72c2418c7ebf54bf7d71d8f58eb8f478bd1d3a
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-03 21:35:27 +0200

    refactor number to number

--- src/test/java/com/codurance/katalyst/FizzBuzzExtendedTest.java
+++ src/test/java/com/codurance/katalyst/FizzBuzzExtendedTest.java
@@ -3,21 +3,15 @@ package com.codurance.katalyst;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FizzBuzzExtendedTest {
     
-    @Test
-    public void convert_one_to_one(){
-        assertEquals("1", new FizzBuzzExtended().convert(1));
+    @ParameterizedTest
+    @CsvSource({ "1,1", "2,2", "4,4" })
+    public void convert_number_to_number(int input, String expected){
+        assertEquals(expected, new FizzBuzzExtended().convert(input));
     }
 
-    @Test
-    public void convert_two_to_two(){
-        assertEquals("2", new FizzBuzzExtended().convert(2));
-    }
-
-    @Test
-    public void convert_four_to_four(){
-        assertEquals("4", new FizzBuzzExtended().convert(4));
-    }
 }
