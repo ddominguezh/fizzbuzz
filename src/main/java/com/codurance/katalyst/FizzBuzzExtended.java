f635af6976e933b8873d56b34d26873a4c191943
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-03 21:40:36 +0200

    convert six to fizz OK

--- src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
+++ src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
@@ -3,6 +3,9 @@ package com.codurance.katalyst;
 public class FizzBuzzExtended {
     
     public String convert(int value){
+        if(value == 6){
+            return "fizz";
+        }
         return String.valueOf(value);
     }
 }
