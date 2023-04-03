2874a49a7ecc99e141c287661a9a56dea06a02d2
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-03 21:27:38 +0200

    convert two to two OK

--- src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
+++ src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
@@ -3,6 +3,9 @@ package com.codurance.katalyst;
 public class FizzBuzzExtended {
     
     public String convert(int value){
+        if(value == 2){
+            return "2";
+        }
         return "1";
     }
 }
