a3490390140d69cc9a9bf0c24af07d6a106aeef6
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-03 21:33:59 +0200

    convert four to four OK

--- src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
+++ src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
@@ -3,9 +3,6 @@ package com.codurance.katalyst;
 public class FizzBuzzExtended {
     
     public String convert(int value){
-        if(value == 2){
-            return "2";
-        }
-        return "1";
+        return String.valueOf(value);
     }
 }
