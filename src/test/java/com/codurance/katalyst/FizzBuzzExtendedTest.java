b25252c6500390a002f336a6574e363460120b90
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-03 21:27:05 +0200

    convert two to two KO

--- src/test/java/com/codurance/katalyst/FizzBuzzExtendedTest.java
+++ src/test/java/com/codurance/katalyst/FizzBuzzExtendedTest.java
@@ -10,4 +10,9 @@ public class FizzBuzzExtendedTest {
     public void convert_one_to_one(){
         assertEquals("1", new FizzBuzzExtended().convert(1));
     }
+
+    @Test
+    public void convert_two_to_two(){
+        assertEquals("2", new FizzBuzzExtended().convert(2));
+    }
 }
