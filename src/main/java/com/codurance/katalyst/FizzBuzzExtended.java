6848119aaca4b0c81a2b20b86cc55993ea214a36
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-04 04:50:45 +0200

    convert five to buzzbuzz OK

--- src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
+++ src/main/java/com/codurance/katalyst/FizzBuzzExtended.java
@@ -4,11 +4,15 @@ public class FizzBuzzExtended {
     
     private static int FIZZ_MODULE = 3;
     private static String FIZZ_VALUE = "fizz";
+    private static String BUZZ_VALUE = "buzz";
 
     public String convert(int value){
         String result = null;
         if(value % FIZZ_MODULE == 0){
             result = FIZZ_VALUE;
+        }
+        if(value == 5){
+            result = BUZZ_VALUE+BUZZ_VALUE;
         }
         for(char number : String.valueOf(value).toCharArray()) {
             if(Character.getNumericValue(number) == FIZZ_MODULE){
